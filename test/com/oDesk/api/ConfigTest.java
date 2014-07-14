a0cf750ecd98f4bf137bed72310de821e6420f8c
Maksym Novozhylov <mnovozhilov@odesk.com>
2014-07-14 14:26:29 +0200

    add base for unit tests and travis

---
+++ test/com/oDesk/api/ConfigTest.java
@@ -0,0 +1,14 @@
+package com.oDesk.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.oDesk.api.Config;
+
+public class ConfigTest {
+	@Test
+	public void testConfig(){
+		assertTrue(1 == 1);
+	}
+}
